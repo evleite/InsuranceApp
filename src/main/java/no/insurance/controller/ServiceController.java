c02d68bb8c8d3a527362b54263275d464304bba9
Pedro Alves <alves.pedro0@gmail.com>
2016-10-22 19:59:29 +0200

    Added RESTfull Service and unit test

---
+++ src/main/java/no/insurance/controller/ServiceController.java
@@ -0,0 +1,51 @@
+package no.insurance.controller;
+
+import no.insurance.domain.Service;
+import no.insurance.repository.ServiceRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by p on 22/10/2016.
+ */
+@RestController
+@RequestMapping("api/v1/")
+public class ServiceController {
+
+    @Autowired
+    private ServiceRepository _serviceRepository;
+
+    @RequestMapping(value = "service", method = RequestMethod.GET)
+    public List<Service> list() {
+        return _serviceRepository.findAll();
+    }
+
+    @RequestMapping(value = "service", method = RequestMethod.POST)
+    public Service create(@RequestBody Service service) {
+        return _serviceRepository.saveAndFlush(service);
+    }
+
+    @RequestMapping(value = "service/{id}", method = RequestMethod.GET)
+    public Service get(@PathVariable Long id) {
+        return _serviceRepository.findOne(id);
+    }
+
+    @RequestMapping(value = "service/{id}", method = RequestMethod.PUT)
+    public Service update(@PathVariable Long id, @RequestBody Service service) {
+        Service existingService = _serviceRepository.findOne(id);
+        BeanUtils.copyProperties(service, existingService);
+        return _serviceRepository.saveAndFlush(existingService);
+    }
+
+    @RequestMapping(value = "service/{id}", method = RequestMethod.DELETE)
+    public Service delete(@PathVariable Long id) {
+        Service existingService = _serviceRepository.findOne(id);
+        _serviceRepository.delete(existingService);
+        return existingService;
+
+        //NOTE: return void is possible also
+    }
+}
--- src/test/java/no/insurance/controller/ServiceControllerTest.java
+++ src/test/java/no/insurance/controller/ServiceControllerTest.java
@@ -3,10 +3,12 @@ package no.insurance.controller;
 import no.insurance.domain.Service;
 import no.insurance.helper.Insurance;
 import no.insurance.helper.State;
+import no.insurance.repository.ServiceRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
@@ -14,18 +16,23 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by p on 22/10/2016.
  */
-public class ServiceStubControllerTest {
+public class ServiceControllerTest {
 
     private Service expectedService;
     private List<Service> expectedServices;
     private Long userId1;
 
     @InjectMocks
-    private ServiceStubController controllerMock;
+    private ServiceController controllerMock;
+
+    @Mock
+    private ServiceRepository _serviceRepositoryMock;
 
     @Before
     public void setUp() throws Exception {
@@ -44,16 +51,21 @@ public class ServiceStubControllerTest {
 
     @Test
     public void testList() throws Exception {
+        //MOCHITO
+        when(_serviceRepositoryMock.findAll()).thenReturn(expectedServices);
 
         List<Service> actualServices = controllerMock.list();
 
         assertNotNull(actualServices);
         assertEquals(expectedServices.toArray().length,1);
 
-        // Method is static
-        assertEquals(actualServices.toArray().length,3);
+        //verify if method was call
+        verify(_serviceRepositoryMock).findAll();
 
         assertEquals( expectedServices.get(0).getId().longValue(), actualServices.get(0).getId().longValue() );
+
+        //Hamcrest
+        //assertThat(expectedServices.get(0).getId(), is(actualServices.getId()));
     }
 
     @Test
